b204471cc079aeea7a1bd47b9a029bef99e7a7b8
Рафиль <qwerty@mail.ru>
2022-04-12 20:16:39 +0300

    Урок26-str,num,bool

---
+++ js/26 - str,num,bool.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// To string
+
+// 1)
+
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2)
+
+console.log(typeof(5 + ''));
+
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
+
+// To Number
+
+// 1)
+
+console.log(typeof(Number('Алиса')));
+
+// 2)
+
+console.log(typeof(+ 'AK'));
+
+// 3)
+
+console.log(typeof(parseInt('15px', 10)));
+
+let answ = +prompt('Hello', '');
+
+// To boolean
+
+// 0, '', null, undefined, NaN; - все что дает false
+
+let switcher = null;
+
+if (switcher) {
+    console.log('Working');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working');
+}
+
+// 2)
+
+console.log(typeof(Boolean('AK')));
+
+// 3)
+
+console.log(typeof(!!'AK'));
